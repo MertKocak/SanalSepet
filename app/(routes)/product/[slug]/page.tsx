dfa615d2aa8c8b15c4ed7d5779d20fdcfd66ec4d
Mert Koçak <mertkocak.2811@gmail.com>
2025-06-27 11:49:01 +0300

    hata4

--- app/(routes)/product/[slug]/page.tsx
+++ app/(routes)/product/[slug]/page.tsx
@@ -1,7 +1,28 @@
-import ProductClient from './ProductClient';
+import { getProducts } from '@/actions/getProducts';
+import { Product } from '@/constans/type';
+import ProductDetail from '../../_components/Product/ProductDetail';
+import ProductDetailSkeleton from '../../_components/Skeleton/ProductDetailSkeleton';
 
-export default async function ProductPage(props: { params: { slug: string } }) {
-  const { slug } = await props.params;
-
-  return <ProductClient slug={slug} />;
+interface ProductDetailPageProps {
+  params: {
+    slug: string;
+  };
 }
+
+const ProductDetailPage = async ({ params }: ProductDetailPageProps) => {
+  const products: Product[] = await getProducts(`/products?filters[slug][$eq]=${params.slug}&populate=*`);
+
+  return (
+    <div className='px-20 xl:px-48'>
+      {products.length === 0 ? (
+        <ProductDetailSkeleton />
+      ) : (
+        products.map((product, index) => (
+          <ProductDetail key={index} product={product} />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default ProductDetailPage;
