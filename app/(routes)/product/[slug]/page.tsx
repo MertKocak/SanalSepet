10cc93be07ff8a2044de6f3d71103d8aaedb7e42
Mert Koçak <mertkocak.2811@gmail.com>
2025-06-27 12:48:01 +0300

    hata9

--- app/(routes)/product/[slug]/page.tsx
+++ app/(routes)/product/[slug]/page.tsx
@@ -1,13 +1,29 @@
-import ProductDetailClient from "./ProductDetailClient";
+// page.tsx
+import { getProducts } from '@/actions/getProducts';
+import ProductDetail from '../../_components/Product/ProductDetail';
+/* import ProductDetailSkeleton from '../../_components/Skeleton/ProductDetailSkeleton'; */
 
-type PageProps = {
+interface ProductDetailPageProps {
   params: {
     slug: string;
   };
-};
+}
 
-const ProductDetailPage = ({ params }: PageProps) => {
-  return <ProductDetailClient slug={params.slug} />;
+const ProductDetailPage = async ({ params }: ProductDetailPageProps) => {
+  // Direkt sunucu tarafında fetch et
+  const products = await getProducts(`/products?filters[slug][$eq]=${params.slug}&populate=*`);
+
+  if (!products || products.length === 0) {
+    return <div>Ürün bulunamadı</div>;
+  }
+
+  return (
+    <div className="px-20 xl:px-48">
+      {products.map((product, index) => (
+        <ProductDetail key={index} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductDetailPage;
