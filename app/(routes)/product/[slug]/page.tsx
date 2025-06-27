fc3cb990503021a4746d9e7d8ef4681585016e2a
Mert Koçak <mertkocak.2811@gmail.com>
2025-06-27 11:58:46 +0300

    hata5

--- app/(routes)/product/[slug]/page.tsx
+++ app/(routes)/product/[slug]/page.tsx
@@ -1,28 +1,47 @@
+'use client';
+
 import { getProducts } from '@/actions/getProducts';
 import { Product } from '@/constans/type';
+import React, { useEffect, useState } from 'react'
 import ProductDetail from '../../_components/Product/ProductDetail';
 import ProductDetailSkeleton from '../../_components/Skeleton/ProductDetailSkeleton';
 
-interface ProductDetailPageProps {
-  params: {
-    slug: string;
-  };
-}
+const ProductDetailPage = ({ params }: { params: { slug: string } }) => {
 
-const ProductDetailPage = async ({ params }: ProductDetailPageProps) => {
-  const products: Product[] = await getProducts(`/products?filters[slug][$eq]=${params.slug}&populate=*`);
+  const [productDetail, setProductDetail] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { slug } = params;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const categories = await getProducts(`/products?filters[slug][$eq]=${slug}&populate=*`);
+        setProductDetail(categories);
+
+      } catch (error) {
+        console.log("Ürünler yüklenirken hata oluştu.", error)
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, [])
 
   return (
-    <div className='px-20 xl:px-48'>
-      {products.length === 0 ? (
+    <>
+      {loading ?
         <ProductDetailSkeleton />
-      ) : (
-        products.map((product, index) => (
-          <ProductDetail key={index} product={product} />
-        ))
-      )}
-    </div>
-  );
-};
+        :
+        <div className='px-20 xl:px-48'>
+          {productDetail.map((product, index) => (
+            <ProductDetail
+              key={index}
+              product={product}
+            />
+          ))}
+        </div>}
+    </>
+  )
+}
 
-export default ProductDetailPage;
+export default ProductDetailPage
