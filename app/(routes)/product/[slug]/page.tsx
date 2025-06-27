2ecaae24f44eaa923ff7c5afc6c9d4b88e98f4ad
Mert Koçak <mertkocak.2811@gmail.com>
2025-06-27 12:09:37 +0300

    hata6

--- app/(routes)/product/[slug]/page.tsx
+++ app/(routes)/product/[slug]/page.tsx
@@ -1,47 +1,11 @@
-'use client';
+import ProductDetailClient from "./ProductDetailClient";
 
-import { getProducts } from '@/actions/getProducts';
-import { Product } from '@/constans/type';
-import React, { useEffect, useState } from 'react'
-import ProductDetail from '../../_components/Product/ProductDetail';
-import ProductDetailSkeleton from '../../_components/Skeleton/ProductDetailSkeleton';
-
-const ProductDetailPage = ({ params }: { params: { slug: string } }) => {
-
-  const [productDetail, setProductDetail] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
-  const { slug } = params;
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const categories = await getProducts(`/products?filters[slug][$eq]=${slug}&populate=*`);
-        setProductDetail(categories);
-
-      } catch (error) {
-        console.log("Ürünler yüklenirken hata oluştu.", error)
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchProducts();
-  }, [])
-
-  return (
-    <>
-      {loading ?
-        <ProductDetailSkeleton />
-        :
-        <div className='px-20 xl:px-48'>
-          {productDetail.map((product, index) => (
-            <ProductDetail
-              key={index}
-              product={product}
-            />
-          ))}
-        </div>}
-    </>
-  )
+interface ProductDetailPageProps {
+  params: { slug: string };
 }
 
-export default ProductDetailPage
+const ProductDetailPage = ({ params }: ProductDetailPageProps) => {
+  return <ProductDetailClient slug={params.slug} />;
+};
+
+export default ProductDetailPage;
