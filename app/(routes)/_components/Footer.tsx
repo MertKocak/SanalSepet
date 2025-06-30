c0f8e4639d5b42e01492aefcebfb4c4dd0a31953
Mert Koçak <mertkocak.2811@gmail.com>
2025-06-30 18:01:36 +0300

    footer düzenlendi

--- app/(routes)/_components/Footer.tsx
+++ app/(routes)/_components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <>
             <footer className='text-center border-t flex flex-col px-20 xl:px-40'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-16 justify-end items-start gap-y-12'>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 py-16 justify-end items-start gap-y-12'>
                     <div className='flex flex-col items-start gap-2 lg:mx-auto'>
                         <p style={{ color: "#ff6700" }} className='font-bold text-lg mb-1'>Kurumsal</p>
                         <Link href={"/"} className='text-sm text-gray-700'>Hakkımızda</Link>
@@ -83,7 +83,7 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-            <Link href={"https://www.mertkocak.online/"} className='border-t py-4 text-center flex flex-col px-20 xl:px-40 text-gray-700 font-medium text-sm'>Developed by Mert Koçak</Link>
+            <Link href={"https://www.mertkocak.online/"} className='border-t py-6 text-center flex flex-col hover:text-orange-500 px-20 xl:px-40 text-gray-700 font-semibold text-sm'>Developed by Mert Koçak</Link>
         </>
     )
 }
